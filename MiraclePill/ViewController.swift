0f6a3980e613cbdf4fe1c9da0a2f07ebcd70407b
Amir Bahadori <pbahadori156@gmail.com>
2016-08-31 17:35:32 -0700

    Added pill imageView and personal info text fields

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Pasha Bahadori on 8/30/16.
+//  Copyright © 2016 Pelican. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
